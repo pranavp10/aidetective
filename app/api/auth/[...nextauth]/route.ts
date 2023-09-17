a34ccd5d7a583e8935de3222f6bae8af94a2c034
Pranav <pranavkp.me@outlook.com>
2023-09-17 14:10:58 +0530

    fix: build

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,26 +1,6 @@
 import NextAuth from "next-auth/next";
-import GithubProvider from 'next-auth/providers/github'
-import GoogleProvider from 'next-auth/providers/google'
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
+import { authOptions } from "@/utils/authOptions";
 
-const prisma = new PrismaClient();
+const handler = NextAuth(authOptions)
 
-export const authOptions = {
- adapter: PrismaAdapter(prisma),
- secret: process.env.NEXTAUTH_SECRET,
- providers: [
-  GithubProvider({
-   clientId: process.env.GITHUB_ID ?? '',
-   clientSecret: process.env.GITHUB_SECRET ?? ""
-  }),
-  GoogleProvider({
-   clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-   clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-  }),
- ]
-}
-
-export const handler = NextAuth(authOptions)
-
-export { handler as GET, handler as Post }
+export { handler as GET, handler as POST }
---
+++ utils/authOptions.ts
@@ -0,0 +1,23 @@
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from 'next-auth/providers/github'
+import GoogleProvider from 'next-auth/providers/google'
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+
+
+export const authOptions: NextAuthOptions = {
+ adapter: PrismaAdapter(prisma),
+ secret: process.env.NEXTAUTH_SECRET,
+ providers: [
+  GithubProvider({
+   clientId: process.env.GITHUB_ID ?? '',
+   clientSecret: process.env.GITHUB_SECRET ?? ""
+  }),
+  GoogleProvider({
+   clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+   clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+  }),
+ ]
+}
