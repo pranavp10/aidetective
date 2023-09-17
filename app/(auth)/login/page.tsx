e70998babb553d0c1fa1648924225f178dbaecaa
Pranav <pranavkp.me@outlook.com>
2023-09-17 21:16:55 +0530

    fix: redirection for authentication

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -6,7 +6,7 @@ const Login = async () => {
   const providers = await getProviders();
 
   return (
-    <div className="h-screen flex items-center justify-centerv">
+    <div className="h-screen flex items-center justify-center">
       <div className="flex justify-center items-center h-full w-1/3 bg-ui-bg-overlay">
         <AuthButton providers={providers} />
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
-import { Button, Heading } from "@medusajs/ui";
+"use client";
+
+import { Heading } from "@medusajs/ui";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { status } = useSession();
+  const { push } = useRouter();
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      push("/dashboard");
+    }
+  }, [status, push]);
+
   return (
     <main className="flex min-h-[calc(100vh-64px)] flex-col items-center justify-center">
       <Heading>Coming soon</Heading>
