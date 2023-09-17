41558515a21ae98c7ae17f7fba0280d45f931870
Pranav <pranavkp.me@outlook.com>
2023-09-17 14:20:55 +0530

    updated the index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@medusajs/ui";
+import { Button, Heading } from "@medusajs/ui";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <Button>Login with github</Button>
+    <main className="flex min-h-[calc(100vh-64px)] flex-col items-center justify-center">
+      <Heading>Coming soon</Heading>
     </main>
   );
 }
