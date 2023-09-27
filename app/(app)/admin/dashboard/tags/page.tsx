8c02c095d3a10c073de027e67001dc87e453930f
Pranav <pranavkp.me@outlook.com>
2023-09-27 13:25:12 +0530

    (feat) export tags csv

--- app/(app)/admin/dashboard/tags/page.tsx
+++ app/(app)/admin/dashboard/tags/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import { ArrowDownTray, ArrowUpTray, Plus, Spinner } from "@medusajs/icons";
-import { Button, Heading } from "@medusajs/ui";
+import { Button, Heading, useToast } from "@medusajs/ui";
 import React from "react";
 import { AddTag } from "./components/addTag";
 import useSWR from "swr";
 import { TagTable } from "./components/tagsList";
-
+import { unparse } from "papaparse";
 const Tag = () => {
+  const { toast } = useToast();
   const { data, error, isLoading } = useSWR<Tag[]>("/api/tags");
   return (
     <div>
@@ -15,7 +16,28 @@ const Tag = () => {
           <Heading>Tag</Heading>
         </div>
         <div className="flex items-center gap-4">
-          <Button variant="secondary">
+          <Button
+            variant="secondary"
+            onClick={() => {
+              if (data) {
+                var csv = unparse(data);
+                var blob = new Blob([csv]);
+                var a = window.document.createElement("a");
+                a.href = window.URL.createObjectURL(blob);
+                a.download = "superflex-tags.csv";
+                document.body.appendChild(a);
+                a.click();
+                document.body.removeChild(a);
+              } else {
+                toast({
+                  title: "Loading!",
+                  description: "data is still loading",
+                  variant: "info",
+                  duration: 2000,
+                });
+              }
+            }}
+          >
             <ArrowDownTray />
             Export Tag
           </Button>
