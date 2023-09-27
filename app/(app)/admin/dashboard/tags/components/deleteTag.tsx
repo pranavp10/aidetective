16a517dc2c2d63d09e5889f41545c6dcddfaa6ec
Pranav <pranavkp.me@outlook.com>
2023-09-27 11:42:25 +0530

    (feat) delete tag from table

---
+++ app/(app)/admin/dashboard/tags/components/deleteTag.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { Button, Prompt, useToast } from "@medusajs/ui";
+import axios from "axios";
+import { useState } from "react";
+import { mutate } from "swr";
+
+export const DeleteTag = ({
+  onClose,
+  open,
+  tagId,
+}: {
+  open: boolean;
+  onClose: () => void;
+  tagId?: string;
+}) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const { toast } = useToast();
+
+  const onDelete = async () => {
+    try {
+      setIsLoading(true);
+      const { data } = await axios.delete<Tags>(`/api/tags/${tagId}`);
+      setIsLoading(false);
+      onClose();
+      mutate<Tags[]>("/api/tags", async (oldData) => {
+        if (oldData)
+          return [...oldData.filter((tag) => tag.tagId !== data.tagId)];
+      });
+      toast({
+        title: "Success",
+        description: "Tag deleted",
+        variant: "success",
+        duration: 2000,
+      });
+    } catch (e: any) {
+      console.log(e.response.data.error);
+      setIsLoading(false);
+      toast({
+        title: "Error",
+        description: "unable to Delete tag",
+        variant: "error",
+        duration: 2000,
+      });
+    }
+  };
+
+  return (
+    <Prompt open={open}>
+      <Prompt.Content>
+        <Prompt.Header>
+          <Prompt.Title>Delete something</Prompt.Title>
+          <Prompt.Description>
+            Are you sure? This cannot be undone.
+          </Prompt.Description>
+        </Prompt.Header>
+        <Prompt.Footer>
+          <Prompt.Cancel disabled={isLoading} onClick={onClose}>
+            Cancel
+          </Prompt.Cancel>
+          <Button variant="danger" onClick={onDelete} isLoading={isLoading}>
+            Delete
+          </Button>
+        </Prompt.Footer>
+      </Prompt.Content>
+    </Prompt>
+  );
+};
--- app/(app)/admin/dashboard/tags/components/tagsList.tsx
+++ app/(app)/admin/dashboard/tags/components/tagsList.tsx
@@ -1,68 +1,82 @@
-import { Table, Button, DropdownMenu, Input } from "@medusajs/ui";
+"use client";
+import { Table, DropdownMenu, Input } from "@medusajs/ui";
 import dayjs from "dayjs";
 import { EllipsisVertical, PencilSquare, Trash } from "@medusajs/icons";
+import { DeleteTag } from "./deleteTag";
+import { useState } from "react";
 
-const TableDemo = ({ tags }: { tags: Tags[] }) => (
-  <div className="mt-3">
-    <div className="flex mb-4 mt-5 px-10 justify-end">
-      <Input
-        placeholder="Search"
-        id="search-input"
-        type="search"
-        size="small"
-      />
+const TableDemo = ({ tags }: { tags: Tags[] }) => {
+  const [deleteId, setDeleteId] = useState<string | undefined>();
+  return (
+    <div className="mt-3">
+      <div className="flex mb-4 mt-5 px-10 justify-end">
+        <Input
+          placeholder="Search"
+          id="search-input"
+          type="search"
+          size="small"
+        />
+      </div>
+      <Table>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>#</Table.HeaderCell>
+            <Table.HeaderCell>Tag Name</Table.HeaderCell>
+            <Table.HeaderCell>Slug</Table.HeaderCell>
+            <Table.HeaderCell>Create At</Table.HeaderCell>
+            <Table.HeaderCell>Updated At</Table.HeaderCell>
+            <Table.HeaderCell></Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {tags?.map((tag) => {
+            return (
+              <Table.Row key={tag.tagId}>
+                <Table.Cell>{tag.tagId}</Table.Cell>
+                <Table.Cell>{tag.name}</Table.Cell>
+                <Table.Cell>{tag.slug}</Table.Cell>
+                <Table.Cell>
+                  {dayjs(tag.createdAt).format("DD/MM/YYYY")}
+                </Table.Cell>
+                <Table.Cell>
+                  {dayjs(tag.updatedAt).format("DD/MM/YYYY")}
+                </Table.Cell>
+                <Table.Cell>
+                  <DropdownMenu>
+                    <DropdownMenu.Trigger>
+                      <EllipsisVertical />
+                    </DropdownMenu.Trigger>
+                    <DropdownMenu.Content
+                      className="flex flex-col justify-start w-fit"
+                      style={{ minWidth: "10px" }}
+                      align="end"
+                    >
+                      <DropdownMenu.Item className="gap-x-2">
+                        <PencilSquare className="text-ui-fg-subtle" />
+                        Edit
+                      </DropdownMenu.Item>
+                      <DropdownMenu.Item
+                        className="gap-x-2"
+                        onClick={() => setDeleteId(tag.tagId)}
+                      >
+                        <Trash className="text-ui-fg-subtle" />
+                        Delete
+                      </DropdownMenu.Item>
+                    </DropdownMenu.Content>
+                  </DropdownMenu>
+                </Table.Cell>
+              </Table.Row>
+            );
+          })}
+          <DeleteTag
+            onClose={() => setDeleteId(undefined)}
+            open={!!deleteId}
+            tagId={deleteId}
+          />
+        </Table.Body>
+      </Table>
     </div>
-    <Table>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell>#</Table.HeaderCell>
-          <Table.HeaderCell>Tag Name</Table.HeaderCell>
-          <Table.HeaderCell>Slug</Table.HeaderCell>
-          <Table.HeaderCell>Create At</Table.HeaderCell>
-          <Table.HeaderCell>Updated At</Table.HeaderCell>
-          <Table.HeaderCell></Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {tags?.map((tag) => {
-          return (
-            <Table.Row key={tag.tagId}>
-              <Table.Cell>{tag.tagId}</Table.Cell>
-              <Table.Cell>{tag.name}</Table.Cell>
-              <Table.Cell>{tag.slug}</Table.Cell>
-              <Table.Cell>
-                {dayjs(tag.createdAt).format("DD/MM/YYYY")}
-              </Table.Cell>
-              <Table.Cell>
-                {dayjs(tag.updatedAt).format("DD/MM/YYYY")}
-              </Table.Cell>
-              <Table.Cell>
-                <DropdownMenu>
-                  <DropdownMenu.Trigger>
-                    <EllipsisVertical />
-                  </DropdownMenu.Trigger>
-                  <DropdownMenu.Content
-                    className="flex flex-col justify-start w-fit"
-                    style={{ minWidth: "10px" }}
-                    align="end"
-                  >
-                    <DropdownMenu.Item className="gap-x-2">
-                      <PencilSquare className="text-ui-fg-subtle" />
-                      Edit
-                    </DropdownMenu.Item>
-                    <DropdownMenu.Item className="gap-x-2">
-                      <Trash className="text-ui-fg-subtle" />
-                      Delete
-                    </DropdownMenu.Item>
-                  </DropdownMenu.Content>
-                </DropdownMenu>
-              </Table.Cell>
-            </Table.Row>
-          );
-        })}
-      </Table.Body>
-    </Table>
-  </div>
-);
+  );
+};
 
 export default TableDemo;
