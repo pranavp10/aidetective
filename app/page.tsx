10c4b54ca996ec43306ddec88667eed649635a76
Pranav <pranavkp.me@outlook.com>
2023-11-09 00:48:43 +0530

    (fix) hide search

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ const Page = async () => {
     return (
       <main className="flex min-h-[calc(100vh-64px)] flex-col items-center justify-center">
         <div className="container px-4 py-3 md:px-8 mx-auto mt-10 max-w-7xl">
-          <div className="flex justify-center w-full mb-10">
+          {/* <div className="flex justify-center w-full mb-10">
             <div className="relative h-full w-full">
               <div className="absolute right-0 h-full p-3">
                 <div className="bg-ui-bg-interactive  flex items-center w-20 justify-center rounded-full h-full">
@@ -30,7 +30,7 @@ const Page = async () => {
                 placeholder="i want to create"
               />
             </div>
-          </div>
+          </div> */}
           <div className="grid grid-cols-3 gap-6">
             {tools.map((tool) => (
               <ToolCard tool={tool} key={tool.toolId} />
