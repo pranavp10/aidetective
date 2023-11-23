8225a578fb085dccfb8a0bc7550f00e3f0e363e4
Pranav <pranavkp.me@outlook.com>
2023-11-23 17:48:53 +0530

    (feat) posthog analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,9 @@ import NavBar from "@/components/layout/navbar";
 import { authOptions } from "@/utils/authOptions";
 import SWRProvider from "@/context/SWRProvider";
 import { ToastProvider } from "@/context/ToastProvider";
-import Script from "next/script";
 import { Footer } from "./component/footer";
+import { PHProvider, PostHogPageview } from "@/context/PostHogProvider";
+import { Suspense } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -49,26 +50,19 @@ export default async function RootLayout({
         <meta name="theme-color" content="#b91d47" />
       </head>
       <body className={`bg-ui-bg-base text-ui-fg-base ${inter.className}`}>
-        <Script id="google-analytics">
-          {`
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-          ga('create', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}, 'auto');
-          ga('send', 'pageview');
-        `}
-        </Script>
-
         <ToastProvider />
-        <SessionProvider session={session}>
-          <SWRProvider>
-            <NavBar />
-            {children}
-            <Footer />
-          </SWRProvider>
-        </SessionProvider>
+        <Suspense>
+          <PostHogPageview />
+        </Suspense>
+        <PHProvider>
+          <SessionProvider session={session}>
+            <SWRProvider>
+              <NavBar />
+              {children}
+              <Footer />
+            </SWRProvider>
+          </SessionProvider>
+        </PHProvider>
       </body>
     </html>
   );
