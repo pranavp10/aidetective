60d0f711a48f2b01508bd835107e7bcd01ad7bf8
Pranav <pranavkp.me@outlook.com>
2023-12-03 12:22:02 +0530

    (fix) updated the layout and login page

--- app/(auth)/login/authButton.tsx
+++ app/(auth)/login/authButton.tsx
@@ -1,36 +1,13 @@
 "use client";
-import { Button } from "@medusajs/ui";
-import { BuiltInProviderType } from "next-auth/providers/index";
-import {
-  ClientSafeProvider,
-  LiteralUnion,
-  signIn,
-  useSession,
-} from "next-auth/react";
-import { motion } from "framer-motion";
+import { Button, Heading } from "@medusajs/ui";
+import { useSession } from "next-auth/react";
 import { useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
+import { Github, Google } from "@medusajs/icons";
+import Link from "next/link";
+import Image from "next/image";
 
-const container = {
-  hidden: { opacity: 1, scale: 0 },
-  visible: {
-    opacity: 1,
-    scale: 1,
-    transition: {
-      delayChildren: 0.3,
-      staggerChildren: 0.2,
-    },
-  },
-};
-
-const AuthButton = ({
-  providers,
-}: {
-  providers: Record<
-    LiteralUnion<BuiltInProviderType, string>,
-    ClientSafeProvider
-  > | null;
-}) => {
+const AuthButton = () => {
   const { status, data } = useSession();
   const { push } = useRouter();
   const searchParams = useSearchParams();
@@ -48,26 +25,15 @@ const AuthButton = ({
   }, [data]);
 
   return (
-    <div>
-      {status === "unauthenticated" && providers && (
-        <motion.div
-          variants={container}
-          initial="hidden"
-          animate="visible"
-          className="grid gap-4"
-        >
-          {Object.values(providers).map((provider) => (
-            <Button
-              onClick={() => {
-                signIn(provider.id);
-              }}
-              key={provider.name}
-            >
-              Continue with {provider.name}
-            </Button>
-          ))}
-        </motion.div>
-      )}
+    <div className="flex gap-4 flex-col">
+      <Button variant="secondary">
+        <Google />
+        Login with Google
+      </Button>
+      <Button variant="secondary">
+        <Github />
+        Login with Github
+      </Button>
     </div>
   );
 };
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,17 +1,25 @@
 import { Heading } from "@medusajs/ui";
-import { getProviders } from "next-auth/react";
 import AuthButton from "./authButton";
+import Link from "next/link";
+import Image from "next/image";
 
-const Login = async () => {
-  const providers = await getProviders();
-
+const Login = () => {
   return (
     <div className="h-screen flex items-center justify-center">
-      <div className="flex justify-center items-center h-full w-1/3 bg-ui-bg-overlay">
-        <AuthButton providers={providers} />
+      <div className="flex justify-center items-center h-full w-1/3">
+        <AuthButton />
       </div>
-      <div className="flex-1 w-full flex justify-center items-center h-full">
-        <Heading>AI Detective</Heading>
+      <div className="flex-1 w-full flex justify-center items-center h-full ">
+        <Link href="/" className="flex items-center gap-3">
+          <Image
+            src="/android-chrome-512x512.png"
+            className="rounded-full"
+            alt="logo of company"
+            width={30}
+            height={30}
+          />
+          <Heading>AI Detective</Heading>
+        </Link>
       </div>
     </div>
   );
