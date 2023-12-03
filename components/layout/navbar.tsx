1d9eb96d6d778e4e49a3897038362ea53cd1dd10
Pranav <pranavkp.me@outlook.com>
2023-12-03 15:45:45 +0530

    (fix) build

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,97 +1,101 @@
 "use client";
-import useShowNavbar from "@/hooks/useShowNavbar";
-import { Avatar, Button, Heading, DropdownMenu } from "@medusajs/ui";
+import { Avatar, Button, Heading, DropdownMenu, Input } from "@medusajs/ui";
 import { signIn, useSession } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { signOut } from "next-auth/react";
 import Image from "next/image";
+import { Google, Plus } from "@medusajs/icons";
 
 const NavBar = () => {
-  const showNaveBar = useShowNavbar();
   const { data: session, status } = useSession();
   const { push } = useRouter();
 
-  if (showNaveBar)
-    return (
-      <nav className="">
-        <div className=" flex items-center justify-between">
-          <Link href="/" className="flex items-center gap-3">
-            <Image
-              src="/android-chrome-512x512.png"
+  return (
+    <nav className="">
+      <div className=" flex items-center justify-between">
+        <Link href="/" className="flex items-center gap-3">
+          <Image
+            src="/android-chrome-512x512.png"
+            className="rounded-full"
+            alt="logo of company"
+            width={30}
+            height={30}
+          />
+          <Heading>AI Detective</Heading>
+        </Link>
+
+        {status === "unauthenticated" && (
+          <div className="flex items-center gap-2">
+            <Button
               className="rounded-full"
-              alt="logo of company"
-              width={30}
-              height={30}
-            />
-            <Heading>AI Detective</Heading>
-          </Link>
-          {status === "unauthenticated" && (
-            <div className="flex items-center">
-              <Button
-                onClick={() =>
-                  signIn("google", { callbackUrl: "/user/tool/submit-tool" })
-                }
-                variant="transparent"
-              >
-                Submit
-              </Button>
-              <Button onClick={() => signIn("google")} variant="transparent">
-                Login
-              </Button>
-            </div>
-          )}
-          {status === "authenticated" && (
-            <div className="flex items-center gap-3">
-              <Button
-                onClick={() => push("/user/tool/submit-tool")}
-                variant="transparent"
-              >
-                Submit
-              </Button>
-              {session.user?.image && session.user.name && (
-                <DropdownMenu>
-                  <DropdownMenu.Trigger>
-                    <Avatar
-                      src={session.user?.image}
-                      fallback={session.user.name[0]}
-                    />
-                  </DropdownMenu.Trigger>
-                  <DropdownMenu.Content
-                    className="flex flex-col justify-start w-fit z-50"
-                    style={{ minWidth: "10px" }}
-                    align="end"
+              variant="primary"
+              onClick={() => {
+                signIn("google", { callbackUrl: "/user/tool/submit-tool" });
+              }}
+            >
+              <Plus />
+              Submit Tool
+            </Button>
+            <Button onClick={() => signIn("google")} variant="transparent">
+              <Google />
+              Login with Google
+            </Button>
+          </div>
+        )}
+        {status === "authenticated" && (
+          <div className="flex items-center gap-3">
+            <Button
+              className="rounded-full"
+              variant="primary"
+              onClick={() => {
+                push("/user/tool/submit-tool");
+              }}
+            >
+              <Plus />
+              Submit Tool
+            </Button>
+            {session.user?.image && session.user.name && (
+              <DropdownMenu>
+                <DropdownMenu.Trigger>
+                  <Avatar
+                    src={session.user?.image}
+                    fallback={session.user.name[0]}
+                  />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content
+                  className="flex flex-col justify-start w-fit z-50"
+                  style={{ minWidth: "10px" }}
+                  align="end"
+                >
+                  <DropdownMenu.Item
+                    className="gap-x-2 cursor-pointer"
+                    onClick={() => push("/dashboard")}
                   >
+                    Dashboard
+                  </DropdownMenu.Item>
+                  {session.user.role === "SUPER_ADMIN" && (
                     <DropdownMenu.Item
                       className="gap-x-2 cursor-pointer"
-                      onClick={() => push("/dashboard")}
+                      onClick={() => push("/admin/dashboard")}
                     >
-                      Dashboard
+                      Super Admin
                     </DropdownMenu.Item>
-                    {session.user.role === "SUPER_ADMIN" && (
-                      <DropdownMenu.Item
-                        className="gap-x-2 cursor-pointer"
-                        onClick={() => push("/admin/dashboard")}
-                      >
-                        Super Admin
-                      </DropdownMenu.Item>
-                    )}
-                    <DropdownMenu.Item
-                      className="gap-x-2 text-ui-fg-error cursor-pointer"
-                      onClick={() => signOut()}
-                    >
-                      Logout
-                    </DropdownMenu.Item>
-                  </DropdownMenu.Content>
-                </DropdownMenu>
-              )}
-            </div>
-          )}
-        </div>
-      </nav>
-    );
-
-  return null;
+                  )}
+                  <DropdownMenu.Item
+                    className="gap-x-2 text-ui-fg-error cursor-pointer"
+                    onClick={() => signOut()}
+                  >
+                    Logout
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu>
+            )}
+          </div>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
