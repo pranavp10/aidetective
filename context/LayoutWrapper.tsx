d6598432e0501c0daeab3f4e26a011884f3c01b9
Pranav <pranavkp.me@outlook.com>
2023-12-03 14:06:56 +0530

    (fix) category not showing tools

--- context/LayoutWrapper.tsx
+++ context/LayoutWrapper.tsx
@@ -18,14 +18,12 @@ export default function LayoutWrapper({
       <div className="grid w-full grid-cols-1 px-0 lg:mx-auto lg:grid-cols-[280px_1fr]">
         <SidebarTags tags={tags} />
         <div className="w-full h-screen overflow-y-auto">
-          <div className="container px-4 py-3 md:px-8 mx-auto max-w-7xl">
+          <div className="px-4 py-3 md:px-8 mx-auto max-w-7xl">
             <NavBar />
-            <div>
-              <div className="my-10">
-                <SubmitTool />
-              </div>
+            <div className="py-8">
+              <SubmitTool />
             </div>
-            {children}
+            <div className="pt-4">{children}</div>
           </div>
         </div>
       </div>
