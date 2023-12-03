7831138ab9457ea99586e06580058f0bd3510f7f
Pranav <pranavkp.me@outlook.com>
2023-12-03 12:24:10 +0530

    (fix) login button not working

--- app/(auth)/login/authButton.tsx
+++ app/(auth)/login/authButton.tsx
@@ -1,11 +1,9 @@
 "use client";
-import { Button, Heading } from "@medusajs/ui";
-import { useSession } from "next-auth/react";
+import { Button } from "@medusajs/ui";
+import { signIn, useSession } from "next-auth/react";
 import { useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Github, Google } from "@medusajs/icons";
-import Link from "next/link";
-import Image from "next/image";
 
 const AuthButton = () => {
   const { status, data } = useSession();
@@ -26,11 +24,11 @@ const AuthButton = () => {
 
   return (
     <div className="flex gap-4 flex-col">
-      <Button variant="secondary">
+      <Button variant="secondary" onClick={() => signIn("google")}>
         <Google />
         Login with Google
       </Button>
-      <Button variant="secondary">
+      <Button variant="secondary" onClick={() => signIn("github")}>
         <Github />
         Login with Github
       </Button>
