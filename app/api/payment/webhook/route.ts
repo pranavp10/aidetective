129828aa6c6578e5b6bd833f0521ba5ff4fd7c8b
Pranav <pranavkp.me@outlook.com>
2023-12-26 18:35:16 +0530

    (fix) updated the sig

--- app/api/payment/webhook/route.ts
+++ app/api/payment/webhook/route.ts
@@ -1,13 +1,12 @@
 import { prisma } from '@/lib/prisma'
 import { getAdminStrip, } from '@/utils/stripe'
 import { NextRequest } from 'next/server'
-import { headers } from "next/headers";
 import Stripe from 'stripe'
 
 export async function POST(request: NextRequest) {
     const body = await request.text();
     const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!;
-    const sig = headers().get("stripe-signature") as string;
+    const sig = request.headers.get('stripe-signature') as string;
     let event: Stripe.Event;
     try {
         event = getAdminStrip().webhooks.constructEvent(body, sig, endpointSecret);
